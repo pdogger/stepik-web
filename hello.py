d900b3f2305cfb8ed76fb31b767688b934a8c180
Daniil Strelnikov <dyustrelnikov@miem.hse.ru>
2023-03-22 14:00:07 +0300

    1.9 again 2

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
 def wsgi_app(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    print("\n".join(env["QUERY_STRING"].split('&')))
     return [bytes("\n".join(env["QUERY_STRING"].split('&')), encoding="utf-8")]
